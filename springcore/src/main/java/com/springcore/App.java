1476677cbe62819eaf8571c518f63a6d1b6620a9
Juhaied <juhaied.hossen@northsouth.edu>
2023-05-14 19:47:09 +0600

    Creating Object Using IoC

--- springcore/src/main/java/com/springcore/App.java
+++ springcore/src/main/java/com/springcore/App.java
@@ -1,13 +1,20 @@
 package com.springcore;
 
-/**
- * Hello world!
- *
- */
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+       ApplicationContext context= new ClassPathXmlApplicationContext("config.xml");
+       
+       // fetching bean
+       // 1						2			3
+       //Name of the class	object Name	context.getBean("The bean which we want");
+       
+       Student student1 =(Student) context.getBean("student1");
+       
+       System.out.println(student1);
     }
 }
